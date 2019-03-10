e288731a156e95c9dc65ebbd8fd670bd97af8dd4
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-03-09 20:19:45 -0600

    Chapter 5 solutions

--- Chapter 5/prime_numbers_list.py
+++ Chapter 5/prime_numbers_list.py
@@ -1,4 +1,4 @@
-#prime numbers
+#prime numbers list
 
 import math
 
@@ -6,7 +6,7 @@ import math
 def is_prime(num):
     prime = 1
     if num == 2 or num == 3:
-        prime = 0
+        prime = num
     for x in range(2, int(math.sqrt(num))+1):
         if num % x == 0:
             prime = 1
@@ -14,17 +14,15 @@ def is_prime(num):
         else:
             prime = 0
     if prime == 0:
-        return True
-    else:
-        return False
+        return num
 
 
 def main():
-    num = int(input('Enter a number: '))
-    if is_prime(num):
-        print("The given number is prime.")
-    else:
-        print("The given number is not prime.")
+    print('Prime numbers between 1-100: ')
+    for num in range(1,101):
+        if is_prime(num):
+            print(is_prime(num))
+
 
 
 if __name__ == '__main__':
