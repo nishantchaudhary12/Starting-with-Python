14d11e3b737303ad1063b8a81e85c2da8f98c25b
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-04-09 18:13:45 -0500

    Chapter 8 solutions

---
+++ Chapter 8/sentenceCapitalizer.py
@@ -0,0 +1,32 @@
+#Sentence Capitalizer
+
+
+def capitalize(user_input):
+    flag = 1
+    new_string = []
+    i = 0
+    #for i in range(len(user_input)):
+    while i < len(user_input):
+        print(user_input[i])
+        while user_input[i] == ' ':
+            new_string.append(user_input[i])
+            i += 1
+        if user_input[i].isalpha() and flag == 1:
+            new_string.append(user_input[i].upper())
+            flag = 0
+        elif user_input[i].isalpha():
+            new_string.append(user_input[i])
+        elif user_input[i] == '.':
+            new_string.append('.')
+            flag = 1
+        i += 1
+    print(new_string)
+    print(''.join(new_string))
+
+
+def main():
+    user_input = input('Enter a string: ')
+    capitalize(user_input)
+
+
+main()
