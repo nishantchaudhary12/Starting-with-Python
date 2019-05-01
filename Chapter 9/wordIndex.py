f5cd291a545725c2f61ef1f0047a9b5adb465415
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-05-01 18:42:26 -0500

    Chapter 9 solutions

--- Chapter 9/wordIndex.py
+++ Chapter 9/wordIndex.py
@@ -1,12 +1,12 @@
 #word index
 
 
-def main():
+def get_index():
     file = open("Kennedy.txt", "r")
     line_num = 1
     word_dict = dict()
     data = file.readline()
-    data = data.rsplit('\n')
+    data = data.rstrip('\n')
     while data != '':
         string = ''
         for each in data:
@@ -14,18 +14,38 @@ def main():
                 string = string + each
             elif each.isspace():
                 string = string + each
-        print(string)
+
         words_list = string.split(' ')
-        print(words_list)
+
         for word in words_list:
             if word not in word_dict:
                 word_dict[word] = [line_num]
             else:
-                word_dict[word].append(line_num)
+                if line_num not in word_dict[word]:
+                    word_dict[word].append(line_num)
         data = file.readline()
         data = data.rstrip('\n')
         line_num += 1
-    print(word_dict)
 
+    file.close()
+
+    return word_dict
+
+
+def write_index(word_index_dict):
+    file = open('index.txt', 'w')
+    for each in word_index_dict:
+        string_new = ''
+        for num in word_index_dict[each]:
+            string_new = string_new + str(num) + ' '
+        line = each + ':' + string_new
+        file.write(line)
+        file.write('\n')
+
+
+
+def main():
+    word_index_dict = get_index()
+    write_index(word_index_dict)
 
 main()
